4f2c3e8150c9c51c0f8f382c303bf4f8dcd6273d
MDW91 <42375451+MDW91@users.noreply.github.com>
2020-04-21 10:55:51 +0200

    H4 projekt - asymetrisk Kryptering
    H4 opgave

---
+++ AsymetriskKrypteringRSAmodtager/AsymetriskKrypteringRSAmodtager/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsymetriskKrypteringRSAmodtager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            //opret og indsæt public/private key (public key indsættes)
+            var rsa = new RSAWithXMLKey();
+            rsa.UpdateRsaKeys();
+
+            Textbox_Exponent.Text = BitConverter.ToString(rsa.GetExponent());
+            Textbox_Modulus.Text = BitConverter.ToString(rsa.GetModulus());
+
+            //indsæt private rsaParametre i tekstbokse
+            SetPrivateData();
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CipherBytes_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            var rsa = new RSAWithXMLKey();
+
+            // Converter hex string til byte array, decrypter og udskriv det decrypterede tekst
+            byte[] texttodecrypt = Textbox_Cipherbytes.Text.Split('-').Select(item => Convert.ToByte(item, 16)).ToArray();
+            Textbox_Decryptedtext.Text = rsa.DecryptfromXml(texttodecrypt);
+        }
+
+        private void Button_Generate_Keys_Click(object sender, RoutedEventArgs e)
+        {
+
+            var rsa = new RSAWithXMLKey();
+            // updater modulus/exponent og udskriv dem som en hexidecimal teksttreng
+            rsa.UpdateRsaKeys();
+
+            Textbox_Modulus.Text = BitConverter.ToString(rsa.GetModulus());
+            Textbox_Exponent.Text = BitConverter.ToString(rsa.GetExponent());
+        }
+
+        private void D_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DP_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DQ_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void InverseQ_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void P_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Q_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void SetPrivateData()
+        {
+            var rsa = new RSAWithXMLKey();
+
+            // hent alt private data og udskriv dem som hexidecimale tekststrenge
+            D.Text = BitConverter.ToString(rsa.GetPrivateData_D());
+            DP.Text = BitConverter.ToString(rsa.GetPrivateData_Dp());
+            DQ.Text = BitConverter.ToString(rsa.GetPrivateData_Dq());
+            InverseQ.Text = BitConverter.ToString(rsa.GetPrivateData_InverseQ());
+            P.Text = BitConverter.ToString(rsa.GetPrivateData_P());
+            Q.Text = BitConverter.ToString(rsa.GetPrivateData_Q());
+        }
+    }
+}
---
+++ AsymetriskKrypteringRSAmodtager/AsymetriskKrypteringRSAmodtager/RSAWithXMLKey.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsymetriskKrypteringRSAmodtager
+{
+    public class RSAWithXMLKey
+    {
+
+        public string DecryptfromXml(byte[] texttodecrypt)
+        {
+            var rsa = new RSAWithXMLKey();
+
+            const string privateKeyPath = "c:\\temp\\privatekey.xml";
+
+            // dekrypter hex-streng
+            var decrypted = rsa.DecryptData(privateKeyPath, texttodecrypt);
+
+            return Encoding.Default.GetString(decrypted);
+
+        }
+
+        public void UpdateRsaKeys()
+        {
+            var rsa = new RSAWithXMLKey();
+
+            const string publicKeyPath = "c:\\temp\\publickey.xml";
+            const string privateKeyPath = "c:\\temp\\privatekey.xml";
+
+            // updater modulus/exponent
+            rsa.AssignNewKey(publicKeyPath, privateKeyPath);
+
+        }
+
+        public byte[] GetPrivateData_D()
+        {
+            var rsa = new RSAWithXMLKey();
+            const string privateKeyPath = "c:\\temp\\privatekey.xml";
+
+            using (var Rsa = new RSACryptoServiceProvider(2048))
+            {
+                Rsa.PersistKeyInCsp = false;
+                Rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+
+                var parameters = Rsa.ExportParameters(true);
+                var D = parameters.D;
+
+
+                return D;
+            }
+        }
+
+        public byte[] GetPrivateData_Dp()
+        {
+            var rsa = new RSAWithXMLKey();
+            const string privateKeyPath = "c:\\temp\\privatekey.xml";
+
+            using (var Rsa = new RSACryptoServiceProvider(2048))
+            {
+                Rsa.PersistKeyInCsp = false;
+                Rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+
+                var parameters = Rsa.ExportParameters(true);
+                var Dp = parameters.DP;
+
+
+                return Dp;
+            }
+        }
+
+        public byte[] GetPrivateData_Dq()
+        {
+            var rsa = new RSAWithXMLKey();
+            const string privateKeyPath = "c:\\temp\\privatekey.xml";
+
+            using (var Rsa = new RSACryptoServiceProvider(2048))
+            {
+                Rsa.PersistKeyInCsp = false;
+                Rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+
+                var parameters = Rsa.ExportParameters(true);
+                var Dq = parameters.DQ;
+
+
+                return Dq;
+            }
+        }
+
+        public byte[] GetPrivateData_InverseQ()
+        {
+            var rsa = new RSAWithXMLKey();
+            const string privateKeyPath = "c:\\temp\\privatekey.xml";
+
+            using (var Rsa = new RSACryptoServiceProvider(2048))
+            {
+                Rsa.PersistKeyInCsp = false;
+                Rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+
+                var parameters = Rsa.ExportParameters(true);
+                var InverseQ = parameters.InverseQ;
+
+                return InverseQ;
+            }
+        }
+
+        public byte[] GetPrivateData_P()
+        {
+            var rsa = new RSAWithXMLKey();
+            const string privateKeyPath = "c:\\temp\\privatekey.xml";
+
+            using (var Rsa = new RSACryptoServiceProvider(2048))
+            {
+                Rsa.PersistKeyInCsp = false;
+                Rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+
+                var parameters = Rsa.ExportParameters(true);
+                var P = parameters.P;
+
+                return P;
+            }
+        }
+
+        public byte[] GetPrivateData_Q()
+        {
+            var rsa = new RSAWithXMLKey();
+            const string privateKeyPath = "c:\\temp\\privatekey.xml";
+
+            using (var Rsa = new RSACryptoServiceProvider(2048))
+            {
+                Rsa.PersistKeyInCsp = false;
+                Rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+
+                var parameters = Rsa.ExportParameters(true);
+                var Q = parameters.Q;
+
+                return Q;
+            }
+        }
+        public byte[] GetModulus()
+        {
+            var rsa = new RSAWithXMLKey();
+            const string publicKeyPath = "c:\\temp\\publickey.xml";
+
+            using (var Rsa = new RSACryptoServiceProvider(2048))
+            {
+                Rsa.PersistKeyInCsp = false;
+
+                // læs data fra c:\\temp\\publickey.xml
+                Rsa.FromXmlString(File.ReadAllText(publicKeyPath));
+
+                // expoter modulus fra provider og retuner den
+                var parameters = Rsa.ExportParameters(false);
+                var modulus = parameters.Modulus;
+
+                return modulus;
+            }
+        }
+
+        public byte[] GetExponent()
+        {
+            var rsa = new RSAWithXMLKey();
+            const string publicKeyPath = "c:\\temp\\publickey.xml";
+
+            using (var Rsa = new RSACryptoServiceProvider(2048))
+            {
+                Rsa.PersistKeyInCsp = false;
+                // læs data fra c:\\temp\\publickey.xml
+                Rsa.FromXmlString(File.ReadAllText(publicKeyPath));
+
+                // expoter exponent fra provider og retuner den
+                var parameters = Rsa.ExportParameters(false);
+                var exponent = parameters.Exponent;
+
+                return exponent;
+            }
+        }
+
+        public void AssignNewKey(string publicKeyPath, string privateKeyPath)
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+
+                rsa.PersistKeyInCsp = false;
+
+                //if (File.Exists(privateKeyPath))
+                //{
+                //    File.Delete(privateKeyPath);
+                //}
+
+                //if (File.Exists(publicKeyPath))
+                //{
+                //    File.Delete(publicKeyPath);
+                //}
+
+                var publicKeyfolder = Path.GetDirectoryName(publicKeyPath);
+                var privateKeyfolder = Path.GetDirectoryName(privateKeyPath);
+
+                if (!Directory.Exists(publicKeyfolder))
+                {
+                    Directory.CreateDirectory(publicKeyfolder);
+                }
+
+                if (!Directory.Exists(privateKeyfolder))
+                {
+                    Directory.CreateDirectory(privateKeyfolder);
+                }
+
+                // skriv rsa object til vores public/private xml filer
+                File.WriteAllText(publicKeyPath, rsa.ToXmlString(false));
+                File.WriteAllText(privateKeyPath, rsa.ToXmlString(true));
+            }
+        }
+
+        public byte[] DecryptData(string privateKeyPath, byte[] dataToEncrypt)
+        {
+            byte[] plain;
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+
+                // læs private key og dekrypter bagefter, retuner plaintekst som byte array
+                rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+                plain = rsa.Decrypt(dataToEncrypt, false);
+            }
+
+            return plain;
+        }
+
+    }
+}
